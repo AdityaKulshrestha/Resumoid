9f9ed9d1aab2420ef7d72cc725a7fe91f61f9cec
Satvik <satvik.pss@gmail.com>
2023-10-13 20:32:13 +0530

    int scores

--- enh.py
+++ enh.py
@@ -147,6 +147,8 @@ def llm_scoring(llm, resume_text, job_description):
 
     Please provide the scores and feedback to the candidate in the following format:
 
+    Note: Score should be integers between 1 to 10. 
+
     Relevant Experience: {{score_experience}}, Feedback: {{feedback_experience}}
     Education: {{score_education}}, Feedback: {{feedback_education}}
     Skills: {{score_skills}}, Feedback: {{feedback_skills}}
